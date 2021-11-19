69bc7c7106f1d8b8571b2f6b555f4e200657f3a5
Bach Tran <bach.tran@revature.com>
2021-11-19 10:22:28 -0600

    Add fuctionality for add to ArithmeticController

--- src/main/java/com/revature/controller/ArithmeticController.java
+++ src/main/java/com/revature/controller/ArithmeticController.java
@@ -1,16 +1,26 @@
 package com.revature.controller;
 
+import com.revature.service.ArithmeticService;
+
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
 public class ArithmeticController {
 
+	private ArithmeticService arithmeticService;
+	
 	private Handler add = (ctx) -> {
+		String num1 = ctx.formParam("number1");
+		String num2 = ctx.formParam("number2");
 		
+		String sum = this.arithmeticService.add(num1, num2);
+		
+		ctx.result(sum);
 	};
 	
 	// Constructor
 	public ArithmeticController() {
+		this.arithmeticService = new ArithmeticService();
 	}
 	
 	public void mapEndpoints(Javalin app) {
